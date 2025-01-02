2c3288d26816048b7f5eb9e5a631aaf7b5b3a737
maridi manoj kumar <21a91a6152@aec.edu.in>
2025-01-02 15:28:32 +0530

    commit5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,9 @@ import express, { NextFunction, Request, Response } from "express";
 import rootRouter from "./routes";
 import bodyParser from "body-parser";
 import cors from "cors";
-import path from 'path';
+import path from "path";
 import { config } from "dotenv";
+
 config();
 
 const app = express();
@@ -11,19 +12,19 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.redirect('/app');
+  res.redirect("/app");
 });
 
-app.use('/app', express.static(path.join(__dirname, '../../frontend/dist'), {maxAge: 0}));
-app.get('/app/*', (req: Request, res: Response) => {
-  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+app.use("/app", express.static(path.join(__dirname, "../../frontend/dist"), { maxAge: 0 }));
+app.get("/app/*", (req: Request, res: Response) => {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, private");
+  res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
 });
 
-app.use('/admin', express.static(path.join(__dirname, '../../admin/dist'), {maxAge: 0}));
-app.get('/admin/*', (req: Request, res: Response) => {
-  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-  res.sendFile(path.join(__dirname, '../../admin/dist', 'index.html'));
+app.use("/admin", express.static(path.join(__dirname, "../../admin/dist"), { maxAge: 0 }));
+app.get("/admin/*", (req: Request, res: Response) => {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, private");
+  res.sendFile(path.join(__dirname, "../../admin/dist", "index.html"));
 });
 
 app.use("/api/v1", rootRouter);
