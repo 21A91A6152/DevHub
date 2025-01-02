5cb77eafa58b205fe877868c5bb9b6eee3b472af
maridi manoj kumar <21a91a6152@aec.edu.in>
2025-01-02 15:21:09 +0530

    commit4

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,7 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "./app";
 
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req as any, res as any);
-};
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
 });
- 
