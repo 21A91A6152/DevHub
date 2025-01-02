4b8b202c71f0f8df8ada1f2a058022641a8eeb77
maridi manoj kumar <21a91a6152@aec.edu.in>
2025-01-02 15:03:19 +0530

    rollback

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,4 @@
-// import { VercelRequest, VercelResponse } from "@vercel/node";
-// import app from "./app";
-
-// export default (req: VercelRequest, res: VercelResponse) => {
-//   app(req as any, res as any);
-// };
-
-// const PORT = process.env.PORT || 3001;
-
-// app.listen(PORT, () => {
-//   console.log(`Server is listening at port ${PORT}`);
-// });
-
+ 
 import app from "./app";
 
 const PORT = process.env.PORT || 3001;
